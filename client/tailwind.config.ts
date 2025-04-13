d44edb5626cdbfb0fb49bc95bcad7d6cfdebf40d
goraria <gorariagroup@gmail.com>
2025-04-13 08:39:37 +0700

    Master - Add base Shadcn Components

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import fontFamily from "tailwindcss/defaultTheme";
 
 export default {
   darkMode: "class",
@@ -7,7 +8,39 @@ export default {
     "./app/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  prefix: "",
   theme: {
+    screens: {
+      xs: "480px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      xxl: "1536px",
+    },
+    fontFamily: {
+      sans: ["var(--font-sans)", ...fontFamily.sans],
+      // sans: ["Inter", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+      mono: ["Menlo", "monospace"],
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "1rem",
+        lg: "2rem",
+        xl: "2rem",
+        xxl: "2rem",
+      },
+      screens: {
+        sm: '100%',
+        md: '100%',
+        lg: '100%',
+        xl: '100%',
+        '2xl': '100%',
+      },
+    },
     extend: {
       colors: {
         background: "var(--background)",
